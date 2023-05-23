442333fb9a3308a1c4d5afad7cb90a52a1da00ad
mhenrique94 <mhenrique94@outlook.com>
2023-05-23 20:25:40 -0300

    adjusts responsiveness and minor things

--- src/components/Home/hero-section.vue
+++ src/components/Home/hero-section.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="inicio">
-    <v-row align="center" justify="center" class="hero" no-gutters>
-      <div class="apps">
-        <a
-          href="https://api.whatsapp.com/send?phone=5515981630646&text=Ol%C3%A1!"
-          ><v-icon icon="mdi-whatsapp" size="32"></v-icon
-        ></a>
-        <a href="https://www.facebook.com/Martelozzo"
-          ><v-icon icon="mdi-facebook" size="32"></v-icon
-        ></a>
-        <a href="https://goo.gl/maps/6YaH5mctsMkS67Ct5?coh=178572&entry=tt"
-          ><v-icon icon="mdi-google-maps" size="32"></v-icon
-        ></a>
-      </div>
-      <v-col cols="12" xl="2" lg="2" />
-      <v-col cols="12" xl="8" lg="8">
-        <v-container class="hero-container full-height logotipo">
-          <h1 class="text-h2 bg-brown pa-3">Martelozzo Móveis Planejados</h1>
-          <p class="text-h3 text-white copacity">
-            <!-- Transformando seu espaço, uma peça de cada vez -->
-            Você sonha, nós construímos
-          </p>
-        </v-container>
-      </v-col>
-      <v-col cols="12" xl="2" lg="2" />
+    <v-row align="center" justify="center" class="full-height hero">
+      <v-container class="hero-container logotipo copacity">
+        <div class="apps">
+          <a
+            href="https://api.whatsapp.com/send?phone=5515981630646&text=Ol%C3%A1!"
+            ><v-icon icon="mdi-whatsapp" size="32"></v-icon
+          ></a>
+          <a href="https://www.facebook.com/Martelozzo"
+            ><v-icon icon="mdi-facebook" size="32"></v-icon
+          ></a>
+          <a href="https://goo.gl/maps/6YaH5mctsMkS67Ct5?coh=178572&entry=tt"
+            ><v-icon icon="mdi-google-maps" size="32"></v-icon
+          ></a>
+        </div>
+        <h1 class="text-h2 bg-brown pa-3">Martelozzo Móveis Planejados</h1>
+        <p class="text-h4 text-white copacity text-md-h3">
+          <!-- Transformando seu espaço, uma peça de cada vez -->
+          Você sonha, nós construímos
+        </p>
+      </v-container>
     </v-row>
   </div>
 </template>
@@ -69,6 +65,7 @@ export default {
   justify-content: flex-end;
   gap: 8px;
   padding: 8px;
+  max-width: 800px;
 }
 
 .apps > a {
--- src/components/Home/who-whe-are-section.vue
+++ src/components/Home/who-whe-are-section.vue
@@ -3,19 +3,12 @@
     <h2 class="text-h2 mb-10">Quem somos</h2>
     <div class="d-flex flex-row">
       <v-container class="text-content">
-        <p>
+        <p class="mb-2">
           Na Martelozzo Móveis Planejados, somos uma equipe de fabricantes de
           móveis apaixonados e dedicados a criar peças bonitas, funcionais e
           personalizadas para nossos clientes. Acreditamos que móveis devem ser
           mais do que apenas um item funcional em sua casa ou escritório; eles
           devem ser uma expressão do seu estilo e personalidade únicos.
-        </p>
-        <p>
-          Esteja você procurando por uma mesa de jantar personalizada, uma
-          estante de livros única ou uma mesa de escritório exclusiva para sua
-          casa, estamos comprometidos em fornecer o melhor produto e serviço
-          possível. Entre em contato conosco hoje para saber mais sobre nossas
-          opções de móveis personalizados e começar a criar sua peça dos sonhos.
         </p>
       </v-container>
       <v-container class="image-content" />
